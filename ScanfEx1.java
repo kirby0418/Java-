145281600c78ba409dc903a743b0df5b7ed91261
kirby(김민주) <126313147+kirby0418@users.noreply.github.com>
2023-03-15 14:53:39 +0900

    Add files via upload
    기본적인 문자, 숫자 입력받기에 대해 배웠다

---
+++ ScanfEx1.java
@@ -0,0 +1,18 @@
+import java.util.*; //1. import문 추가
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+//		int num = scanner.nextInt();
+//		int num_2 = scanner.nextInt();
+//		System.out.println(num);
+//		System.out.println(num_2);
+		
+		String input = scanner.nextLine(); //line단위로 입력받기
+		int num_3 = Integer.parseInt(input);
+		System.out.println(num_3);
+	}
+
+}
